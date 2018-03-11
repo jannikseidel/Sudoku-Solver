a021c6a7f6a498a3c4c86b0864539a0b90cb5762
Jannik <jannikseidel@users.noreply.github.com>
2018-03-11 20:03:51 +0100

    Changes in the code for rows and coloumns

--- solver.py
+++ solver.py
@@ -9,95 +9,18 @@ import numpy as np
 
 sudoku = np.matrix([[2,3,1,0,0,5,9,0,0],[0,4,9,0,0,0,6,2,0],[0,0,0,0,1,0,0,0,3],[9,0,0,5,3,0,0,7,0],[0,0,8,9,7,4,1,0,0],[1,5,0,0,0,6,0,0,9],[7,0,0,0,6,0,0,0,0],[6,1,0,0,0,2,8,5,0],[0,0,2,1,0,0,0,9,6]])
 
-# VERWENDE STRINGS UM DAS GANZE EFFIZIENTER UND SCHNELLER ZU MACHEN!!!
-
-
-
-str(matrix_1_1[2]).strip('[]')
-
-def solver_3x3(matrix):
-    ''' Returns either a solved matrix if only one number is missing or
-        'more than one' if more than one position is 0
-    '''
-    empty = 0
-    pos_0_x = 0
-    pos_0_y = 0
-
-    pos_1 = False
-    pos_2 = False
-    pos_3 = False
-    pos_4 = False
-    pos_5 = False
-    pos_6 = False
-    pos_7 = False
-    pos_8 = False
-    pos_9 = False
-
-    # find the position of missing number or quit if more than one number missing
-    for x in range(0,3):
-        for y in range(0,3):
-            value = matrix[x,y]
-            if value == 0:
-                empty += 1
-                if empty > 1:
-                    return 'more than one'
-                pos_0_x = x
-                pos_0_y = y
-            elif value == 1:
-                pos_1 = True
-            elif value == 2:
-                pos_2 = True
-            elif value == 3:
-                pos_3 = True
-            elif value == 4:
-                pos_4 = True
-            elif value == 5:
-                pos_5 = True
-            elif value == 5:
-                pos_5 = True
-            elif value == 6:
-                pos_6 = True
-            elif value == 7:
-                pos_7 = True
-            elif value == 8:
-                pos_8 = True
-            elif value == 9:
-                pos_9 = True
-            else:
-                pass
-
-    if not pos_1:
-        matrix[pos_0_x,pos_0_y] = 1
-    elif not pos_2:
-        matrix[pos_0_x,pos_0_y] = 2
-    elif not pos_3:
-        matrix[pos_0_x,pos_0_y] = 3
-    elif not pos_4:
-        matrix[pos_0_x,pos_0_y] = 4
-    elif not pos_5:
-        matrix[pos_0_x,pos_0_y] = 5
-    elif not pos_6:
-        matrix[pos_0_x,pos_0_y] = 6
-    elif not pos_7:
-        matrix[pos_0_x,pos_0_y] = 7
-    elif not pos_8:
-        matrix[pos_0_x,pos_0_y] = 8
-    elif not pos_9:
-        matrix[pos_0_x,pos_0_y] = 9
-
-    return matrix
 
 # Initializing counter for zeros
-count_zero = 1
+zeros = 1
 
-while count_zero > 0:
+while zeros > 0:
     # setting counter to zero
-    count_zero = 0
+    zeros = 0
 
     coloumns = {}
     coloumn_names = ['coloumn_1', 'coloumn_2', 'coloumn_3','coloumn_4', 'coloumn_5', 'coloumn_6','coloumn_7', 'coloumn_8', 'coloumn_9']
 
-    #for loop over the coloumns extracting the numbers as strings
+    # for loop over the coloumns extracting the numbers as strings
     for col_num in range(0,9):
         coloumn = ''
         for col_pos in range(0,9):
@@ -105,16 +28,19 @@ while count_zero > 0:
 
         coloumns[coloumn_names[col_num]] = coloumn
 
-
     row_names = ['row_1', 'row_2', 'row_3','row_4', 'row_5', 'row_6','row_7', 'row_8', 'row_9']
     rows = {}
 
-    #for loop over the rows extracting the numbers as strings
+    # for loop over the rows extracting the numbers as strings and counting the
+    # 0s in them
+
     for row_num in range(0,9):
         row = ''
         for row_pos in range(0,9):
              row = row + str(sudoku[row_num, row_pos])
-
+        zeros = zeros + row.count('0')
+        row = row.replace('0','')
+        print(row)
         rows[row_names[row_num]] = row
 
     # Matrixes are ordered in the following way: Matrix_row_coloumn, the sudoku matrix is composed of 3x3 submatrices
@@ -145,6 +71,7 @@ while count_zero > 0:
                 matrices[matrix] = matrix_slice
             elif matrix[-3] == '3' and matrix[-1] == '2':
                 matrix_slice = np.matrix([[sudoku[6,3],sudoku[6,4],sudoku[6,5]],[sudoku[7,3],sudoku[7,4],sudoku[7,5]],[sudoku[8,3],sudoku[8,4],sudoku[8,5]]])
+                matrices[matrix] = matrix_slice
             elif matrix[-3] == '3' and matrix[-1] == '3':
                 matrix_slice = np.matrix([[sudoku[6,6],sudoku[6,7],sudoku[6,8]],[sudoku[7,6],sudoku[7,7],sudoku[7,8]],[sudoku[8,6],sudoku[8,7],sudoku[8,8]]])
                 matrices[matrix] = matrix_slice
@@ -152,75 +79,200 @@ while count_zero > 0:
     # looking for a single missing number in the coloumns and adding if only
     # one is missing
 
-    for coloumn in coloumn_names:
-        zeros = coloumns[coloumn].count('0')
-        if zeros == 1:
-            position_0 = coloumns[coloumn].find('0')
-            sudoku_col = int(coloumn[-1])-1
+    for coloumn_name in coloumn_names:
+        coloumn = coloumns[coloumn_name]
+        if len(coloumn) == 8:
+            value = 0
             if '1' not in coloumn:
-                sudoku[position_0,sudoku_col] = 1
+                value = 1
             elif '2' not in coloumn:
-                sudoku[position_0,sudoku_col] = 2
+                value = 2
             elif '3' not in coloumn:
-                sudoku[position_0,sudoku_col] = 3
+                value = 3
             elif '4' not in coloumn:
-                sudoku[position_0,sudoku_col] = 4
+                value = 4
             elif '5' not in coloumn:
-                sudoku[position_0,sudoku_col] = 5
+                value = 5
             elif '6' not in coloumn:
-                sudoku[position_0,sudoku_col] = 6
+                value = 6
             elif '7' not in coloumn:
-                sudoku[position_0,sudoku_col] = 7
+                value = 7
             elif '8' not in coloumn:
-                sudoku[position_0,sudoku_col] = 8
+                value = 8
             elif '9' not in coloumn:
-                sudoku[position_0,sudoku_col] = 9
-        else:
-            pass
+                value = 9
 
-    # looking for a single missing number in the rows and adding if only one is
-    # missing
+            col_num = coloumn_name[-1]
 
-    for row in row_names:
-        zeros = rows[row].count('0')
-        print(zeros)
-        if zeros == 1:
-            position_0 = rows[rows].find('0')
-            sudoku_row = int(row[-1])-1
+            if col_num == '1':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,0])
+                row_position = col_values.find('0')
+                sudoku[row_position, 0] = 1
+            elif col_num == '2':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,1])
+                row_position = col_values.find('0')
+                sudoku[row_position, 1] = 2
+            elif col_num == '3':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,2])
+                row_position = col_values.find('0')
+                sudoku[row_position, 2] = 3
+            elif col_num == '4':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,3])
+                row_position = col_values.find('0')
+                sudoku[row_position, 3] = 4
+            elif col_num == '5':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,4])
+                row_position = col_values.find('0')
+                sudoku[row_position, 4] = 5
+            elif col_num == '6':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,5])
+                row_position = col_values.find('0')
+                sudoku[row_position, 5] = 6
+            elif col_num == '7':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,6])
+                row_position = col_values.find('0')
+                sudoku[row_position, 6] = 7
+            elif col_num == '8':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,7])
+                row_position = col_values.find('0')
+                sudoku[row_position, 7] = 8
+            elif col_num == '9':
+                row_position = 0
+                col_values = ''
+                for position in range(0,8):
+                    col_values += str(sudoku[postion ,8])
+                row_position = col_values.find('0')
+                sudoku[row_position, 8] = 9
+
+    # looking for a single missing number in the rows and adding if only
+    # one is missing
+
+    for row_name in row_names:
+        row = rows[row_name]
+        if len(row) == 8:
+            value = 0
             if '1' not in row:
-                sudoku[sudoku_row, position_0] = 1
+                value = 1
             elif '2' not in row:
-                sudoku[sudoku_row, position_0] = 2
+                value = 2
             elif '3' not in row:
-                sudoku[sudoku_row, position_0] = 3
+                value = 3
             elif '4' not in row:
-                sudoku[sudoku_row, position_0] = 4
+                value = 4
             elif '5' not in row:
-                sudoku[sudoku_row, position_0] = 5
+                value = 5
             elif '6' not in row:
-                sudoku[sudoku_row, position_0] = 6
+                value = 6
             elif '7' not in row:
-                sudoku[sudoku_row, position_0] = 7
+                value = 7
             elif '8' not in row:
-                sudoku[sudoku_row, position_0] = 8
+                value = 8
             elif '9' not in row:
-                sudoku[sudoku_row, position_0] = 9
-        else:
-            pass
+                value = 9
+
+            row_num = row_name[-1]
+
+            if row_num == '1':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[0,postion])
+                col_position = row_values.find('0')
+                sudoku[0, col_position] = 1
+            elif row_num == '2':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[1,postion])
+                col_position = row_values.find('0')
+                sudoku[1, col_position] = 2
+            elif col_num == '3':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[2,postion])
+                col_position = row_values.find('0')
+                sudoku[2, col_position] = 3
+            elif col_num == '4':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[3,postion])
+                col_position = row_values.find('0')
+                sudoku[3, col_position] = 4
+            elif col_num == '5':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[4,postion])
+                col_position = row_values.find('0')
+                sudoku[4, col_position] = 5
+            elif col_num == '6':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[5,postion])
+                col_position = row_values.find('0')
+                sudoku[5, col_position] = 6
+            elif col_num == '7':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[6,postion])
+                col_position = row_values.find('0')
+                sudoku[6, col_position] = 7
+            elif col_num == '8':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[7,postion])
+                col_position = row_values.find('0')
+                sudoku[7, col_position] = 8
+            elif col_num == '9':
+                col_position = 0
+                row_values = ''
+                for position in range(0,8):
+                    row_values += str(sudoku[8,postion])
+                col_position = row_values.find('0')
+                sudoku[8, col_position] = 9
+
+    # check if a submatrix is missing a single number, if yes enter this in the
+    # sudoku matrix
+
+    for matrix in matrix_names:
+        sub_matrix = matrices[matrix]
+        numbers_in_matrix = ''
+
+        for line in sub_matrix:
+            numbers_in_matrix = numbers_in_matrix + str(line).strip('[]')
+
+        numbers_in_matrix = numbers_in_matrix.replace('0','')
+        numbers_in_matrix = numbers_in_matrix.replace(' ','')
+        print(numbers_in_matrix)
 
 
     break
-
-
-
-
-
-
-
-
-
-
-
-
-    # counting the zeros in the matrix
-    count_zero = coloumn_1.count('0') + coloumn_2.count('0') + coloumn_3.count('0') + coloumn_4.count('0') + coloumn_5.count('0') + coloumn_6.count('0') + coloumn_7.count('0') + coloumn_8.count('0') + coloumn_9.count('0')
